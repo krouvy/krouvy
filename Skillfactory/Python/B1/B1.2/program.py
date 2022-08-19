32f5ccbba58991b1930cfb19f344fb5bc4047004
BookLove <krouvy@mail.ru>
2022-08-19 18:39:26 +0300

    fwfw

--- Skillfactory/Python/B1/B1.2/program.py
+++ Skillfactory/Python/B1/B1.2/program.py
@@ -1,7 +1,11 @@
 print('hello World')
 print('hello World')
 print('hello World')
+
+maka = 5
+baka = 10
+
+
+print('result', maka+baka)
 print('hello World')
 print('hello World')
-print('hello World')
-    
