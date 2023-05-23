054b7189c0ca2f9e256970481f5ed5db63ad7953
Lexus <krouvy@mail.ru>
2023-05-23 21:50:15 +0300

    Ленюсь не слишком сильно.

---
+++ Skillfactory/Python/B9/B9_4/p3.py
@@ -0,0 +1,18 @@
+pars = {")": "(", "]": "[", '}': '{'}
+
+
+def par_checker_mod(string):
+    stack = []
+
+    for s in string:
+        if s in "([{":
+            stack.append(s)
+        elif s in ")]}":
+            if len(stack) > 0 and stack[-1] == pars[s]:
+                stack.pop()
+            else:
+                return False
+    return len(stack) == 0
+
+string = '{[]()}'
+print(par_checker_mod(string))
