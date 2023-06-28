dffa467944ae8e2faf3087a7e242919da0bad433
Lexus <krouvy@mail.ru>
2023-06-28 19:28:05 +0300

    Чуть-чуть. Но мозги проветрил.

---
+++ Skillfactory/Python/B9/B9_5/p4.py
@@ -0,0 +1,60 @@
+class binaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left_child = None
+        self.right_child = None
+
+    def insert_left(self, next_value):
+        if self.left_child is None:
+            self.left_child = binaryTree(next_value)
+        else:
+            new_child = binaryTree(next_value)
+            new_child.left_child = self.left_child
+            self.left_child = new_child
+        return self
+
+    def insert_right(self, next_value):
+        if self.right_child is None:
+            self.right_child = binaryTree(next_value)
+        else:
+            new_child = binaryTree(next_value)
+            new_child.right_child = self.right_child
+            self.right_child = new_child
+        return self
+
+    def pre_order(self):
+        print(self.value)  # процедура обработки
+
+        if self.left_child is not None:  # если левый потомок существует
+            self.left_child.pre_order()  # рекурсивно вызываем функцию
+
+        if self.right_child is not None:  # если правый потомок существует
+            self.right_child.pre_order()  # рекурсивно вызываем функцию
+
+    def post_order(self):
+
+        if self.left_child is not None:  # если левый потомок существует
+            self.left_child.post_order()  # рекурсивно вызываем функцию
+
+        if self.right_child is not None:  # если правый потомок существует
+            self.right_child.post_order()  # рекурсивно вызываем функцию
+        print(self.value)  # процедура обработки
+
+    def in_order(self):
+
+        if self.left_child is not None:  # если левый потомок существует
+            self.left_child.in_order()  # рекурсивно вызываем функцию
+
+        print(self.value)  # процедура обработки
+
+        if self.right_child is not None:  # если правый потомок существует
+            self.right_child.in_order()  # рекурсивно вызываем функцию
+
+A_node = binaryTree(2).insert_left(7).insert_right(5)
+A_node.left_child.insert_left(2).insert_right(6)
+A_node.left_child.right_child.insert_left(5).insert_right(11)
+A_node.right_child.insert_right(9)
+A_node.right_child.right_child.insert_left(4)
+A_node.right_child.right_child.insert_right(17)
+
+A_node.in_order()
